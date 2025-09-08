10480a6cbeb162a0e6c6bb843b4cacf8c13f0767
Eduardo0310 <ed555203@gmail.com>
2025-09-08 19:45:02 -0300

    Add files via upload

---
+++ Pratica 1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "ola, mundo!"
+
+if __name__=="__main__":
+    app.run(debug=True)
