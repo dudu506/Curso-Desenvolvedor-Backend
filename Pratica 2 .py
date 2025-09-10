1f9a1e56e9f9a533746f9397340a6a197e960551
Eduardo0310 <ed555203@gmail.com>
2025-09-10 20:49:51 -0300

    Add files via upload

---
+++ Pratica 2 .py
@@ -0,0 +1,29 @@
+# Calcule a soma de 15+27
+a = 15
+b = 27
+soma= (a+b)
+print(soma)
+
+# Multiplique 12 por 8
+a = 12
+b = 8
+Multiplicação= (a*b)
+print(Multiplicação)
+
+# Divida 25 por 4 e veja o resultado
+a = 25
+b = 4
+Divisão= (a/b)
+print(Divisão)
+
+# Descubra o resto da divisão de 37 por 5
+a = 37
+b = 5
+Resto= (a%b)
+print(Resto)
+
+# Eleve 3 a potencia de 4
+a = 3
+b = 4
+Potencia= (a**b)
+print(Potencia)
