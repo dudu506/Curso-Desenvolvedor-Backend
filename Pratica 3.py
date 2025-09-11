33e60c4b6e67f82461166359487ddeee4f14b24a
Eduardo0310 <ed555203@gmail.com>
2025-09-11 20:39:50 -0300

    Add files via upload

---
+++ Pratica 3.py
@@ -0,0 +1,30 @@
+#DESAFIO USANDO OPERADORES
+
+#1
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+
+#2
+a = 100
+calcule_tempo= (a - idade)
+print(" a sua idade daqui a 100 anos:",calcule_tempo)
+ 
+#3
+e = 18
+maioridade= (idade>=18)
+print('Maioridade:',maioridade)
+
+#4
+d = [13,14,15,16,17,18,19]
+adolescente= ( idade in d)
+print('adolescente:', adolescente)
+
+#5
+u = idade
+aumento= (u+5)
+print('Aumento de idade:', aumento)
+
+#6
+t = ["eduardo"]
+Teste= ("a" in nome)
+print('Verificando se a letra "a" está no nome:', Teste)
