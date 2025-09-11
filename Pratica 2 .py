612057d392e75bc45659b8b27ca7964bccbcf380
Eduardo0310 <ed555203@gmail.com>
2025-09-10 21:16:30 -0300

    Add files via upload

--- Pratica 2 .py
+++ Pratica 2 .py
@@ -2,28 +2,52 @@
 a = 15
 b = 27
 soma= (a+b)
-print(soma)
+print('Resultado 1:', soma)
 
 # Multiplique 12 por 8
 a = 12
 b = 8
 Multiplicação= (a*b)
-print(Multiplicação)
+print('Resultado 2:', Multiplicação)
 
 # Divida 25 por 4 e veja o resultado
 a = 25
 b = 4
 Divisão= (a/b)
-print(Divisão)
+print('Resultado 3:', Divisão)
 
 # Descubra o resto da divisão de 37 por 5
 a = 37
 b = 5
 Resto= (a%b)
-print(Resto)
+print('Resultado 4:', Resto)
 
 # Eleve 3 a potencia de 4
 a = 3
 b = 4
 Potencia= (a**b)
-print(Potencia)
+print('Resultado 5:', Potencia)
+
+# Verifique se 10 é maior que 7
+a = 10
+b =7
+Verifique= (a>b)
+print('Resultado 6:', Verifique)
+
+# Verifique se 15 é igual a 20
+a = 15
+b = 20
+Verifique= (a==b)
+print('Resultado 7:', Verifique)
+
+# teste se 100 é menor pu igual a 100
+a = 100
+b = 100
+Teste= (a<=b)
+print('Resultado 8:', Teste)
+
+# Veja se 45 é diferente de 30
+a = 45
+b = 30
+Diferença= (a!=b)
+print('Resultado 9:', Diferença)
