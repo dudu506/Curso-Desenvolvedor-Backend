77653e15e90fc8146bb93446374f29128ef9a035
Eduardo0310 <ed555203@gmail.com>
2025-09-17 18:44:39 -0300

    Add files via upload

--- Pratica 7.py
+++ Pratica 7.py
@@ -1,4 +1,5 @@
 #tuplas
+"""#
 minha_tuplas = (1,2,3)
 
 tupla_com_um = (5)
@@ -8,8 +9,8 @@ x,y,z = (1,2,3)
 print(x,y,z)
 
 t = (1,2,2,2,3,5,4)
-print(t.count(2)) # serve para contar quantas vezea o número se repete
-print(t.index(3))  # sece para saber qual posicão vai estar o número
+print(t.count(2)) #serve para contar quantas vezes o número se repete
+print(t.index(3))  #serve  para saber qual posicão vai estar o número
 print(len(t))
 
 #test
@@ -54,12 +55,27 @@ numeros =[1,2,2,3,4,5,6]
 conj = set(numeros)
 print(conj)
 
-lista1 =["Ana","Carlos","joão"]
-lista2 =["maria","Carlos","Pedro"]
+lista1 =["Juliana","Bianca","Sofia"]
+lista2 =["Bianca","Carlos","Amanda",]
 
 conj1 = set (lista1)
 conj2 = set (lista2)
 
 comum = conj1.intersection(conj2)
 
+comum = (conj1-conj2)
 print(comum)
+
+
+
+Frase = input("Peça ao usuario uma frase: ")
+variavel= Frase.split()
+print(variavel)
+
+conj= set(variavel)
+print(conj)
+
+"""#
+Palavra = input("digite uma palavra: ")
+print(len(set(Palavra)))
+print(len(Palavra))
