f456676cdb7daf5668f0bad6f5603d47cf3cadc3
Eduardo0310 <ed555203@gmail.com>
2025-09-17 21:51:45 -0300

    Add files via upload

--- Pratica 8.py
+++ Pratica 8.py
@@ -1,6 +1,11 @@
 #funções
 
-def greet():
-    print('Hello word!')
-  
-    greet()
+def saudação(nome):
+ print(f"Olá,{nome}!")
+saudação("eduardo")
+
+def soma (a,b):
+ return a + b
+resultado = soma (3,5)
+print(resultado)
+ 
