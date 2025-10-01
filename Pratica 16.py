8be5f2eff0b55f5a116c658400c2ef4b25b422cf
Eduardo0310 <ed555203@gmail.com>
2025-10-01 18:57:40 -0300

    Add files via upload

--- Pratica 16.py
+++ Pratica 16.py
@@ -1,19 +1,37 @@
 #Abstração e polimorfismo
 
 from abc import ABC, abstractmethod
-
+"""
 class Instrumento(ABC):
     @abstractmethod
-    def instrumento(self):
+    def tocar(self):
         pass
 
-class violão(Instrumento):
+class Violão(Instrumento):
     def tocar(self):
-        return "O violão começoe a tocar!"
+        return "O violão começou a tocar!"
     
-class bateria(violão):
-    def fazer_som(self):
+class bateria(Instrumento):
+    def tocar(self):
         return "A bateria esta tocando!"        
-instrumentos = [violão(), bateria()]
-for t in instrumentos:
-    print(t.mover())
+instrumentos = [Violão(), bateria()]
+for  i in instrumentos:
+    print(i.tocar())
+"""
+
+class funcionario(ABC,):
+    @abstractmethod
+    def calcular_pg(self):
+      pass
+
+class Assalariado(funcionario):
+    def calcular_pg(self):
+        return f"esse é o seu salario fixo 3500"
+
+class Horista(funcionario):
+    def calcular_pg(self,):
+        return  "o seu salario é R$13 por hora e durente 9 horas de trabalho seu salario será de R$117"
+    
+funcionario = [Assalariado(), Horista()]
+for i in funcionario:
+    print(i.calcular_pg())
